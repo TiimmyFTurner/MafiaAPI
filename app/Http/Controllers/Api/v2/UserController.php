e4fa8ee6908ebf4a8761c8799e229fa4bd62cc02
Arman Askari <armanaskariofficial@gmail.com>
2021-02-26 21:52:41 +0330

    show update delete user

--- app/Http/Controllers/Api/v2/UserController.php
+++ app/Http/Controllers/Api/v2/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\v2;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -25,7 +26,6 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
     }
 
     /**
@@ -36,7 +36,13 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        //
+        if (auth()->id() == $id) {
+            return response()->json([
+                'status' => 200,
+                'data' => ['user' => User::find($id)],
+            ]);
+        } else
+            abort(403, 'Access Denied');
     }
 
     /**
@@ -48,7 +54,24 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        try {
+            if (auth()->id() == $id) {
+                $request->validate(['name' => 'required']);
+                User::find($id)->update(['name' => $request->name]);
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'user updated',
+                    'data' => ['user' => User::find($id)],
+                ]);
+            } else
+                abort(403, 'Access Denied');
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 500,
+                'message' => 'error in updating user',
+                'error' => $th
+            ]);
+        }
     }
 
     /**
@@ -59,6 +82,21 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        try {
+            if (auth()->id() == $id) {
+                User::destroy($id);
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'user deleted',
+                ]);
+            } else
+                abort(403, 'Access Denied');
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 500,
+                'message' => 'error on deleting user',
+                'error' => $th
+            ]);
+        }
     }
 }
