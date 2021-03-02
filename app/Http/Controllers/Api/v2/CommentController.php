542a7bc605c8e4c49bda657e0327ca401275e823
Arman Askari <armanaskariofficial@gmail.com>
2021-03-02 17:02:36 +0330

    store and destroy comment

--- app/Http/Controllers/Api/v2/CommentController.php
+++ app/Http/Controllers/Api/v2/CommentController.php
@@ -3,19 +3,11 @@
 namespace App\Http\Controllers\Api\v2;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comment;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -25,32 +17,32 @@ class CommentController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        try {
+            $request->validate([
+                'event_id' => 'required',
+                'body' => 'required'
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
+            ]);
+            $comment = Comment::create([
+                'body' => $request->body,
+                'event_id' => $request->event_id,
+                'user_id' => auth()->id(),
+            ]);
+            return response()->json([
+                'status' => 200,
+                'data' => [
+                    'message' => 'saccess',
+                    'connection' => $comment
+                ]
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 500,
+                'message' => 'Error in store',
+                'error' => $th,
+            ]);
+        }
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
     /**
      * Remove the specified resource from storage.
      *
@@ -59,6 +51,20 @@ class CommentController extends Controller
      */
     public function destroy($id)
     {
-        //
+        try {
+            if (auth()->id() == Comment::find($id)->user_id) {
+                Comment::find($id)->delete();
+                return response()->json([
+                    'status' => 200,
+                    'data' => ['message' => 'delete saccessful'],
+                ]);
+            } else abort(403, 'Access Denied');
+        } catch (\Exception $error) {
+            return response()->json([
+                'status' => 500,
+                'message' => 'Error in delete',
+                'error' => $error,
+            ]);
+        }
     }
 }
